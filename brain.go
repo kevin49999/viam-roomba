78dd915179f5228f02510228b693ce7debca4abd
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-03-02 08:41:08 -0500

    make sensor readings configurable; add brain service

---
+++ brain.go
@@ -0,0 +1,104 @@
+package viamroomba
+
+import (
+	"context"
+	"fmt"
+
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/resource"
+	generic "go.viam.com/rdk/services/generic"
+)
+
+var (
+	Brain = resource.NewModel("jalen", "viam-roomba", "brain")
+)
+
+func init() {
+	resource.RegisterService(generic.API, Brain,
+		resource.Registration[resource.Resource, *BrainConfig]{
+			Constructor: newBrainBrain,
+		},
+	)
+}
+
+type BrainConfig struct {
+	/*
+		Put config attributes here. There should be public/exported fields
+		with a `json` parameter at the end of each attribute.
+
+		Example config struct:
+			type Config struct {
+				Pin   string `json:"pin"`
+				Board string `json:"board"`
+				MinDeg *float64 `json:"min_angle_deg,omitempty"`
+			}
+
+		If your model does not need a config, replace *Config in the init
+		function with resource.NoNativeConfig
+	*/
+}
+
+// Validate ensures all parts of the config are valid and important fields exist.
+// Returns three values:
+//  1. Required dependencies: other resources that must exist for this resource to work.
+//  2. Optional dependencies: other resources that may exist but are not required.
+//  3. An error if any Config fields are missing or invalid.
+//
+// The `path` parameter indicates
+// where this resource appears in the machine's JSON configuration
+// (for example, "components.0"). You can use it in error messages
+// to indicate which resource has a problem.
+func (cfg *BrainConfig) Validate(path string) ([]string, []string, error) {
+	// Add config validation code here
+	return nil, nil, nil
+}
+
+type brainBrain struct {
+	resource.AlwaysRebuild
+
+	name resource.Name
+
+	logger logging.Logger
+	cfg    *BrainConfig
+
+	cancelCtx  context.Context
+	cancelFunc func()
+}
+
+func newBrainBrain(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (resource.Resource, error) {
+	conf, err := resource.NativeConfig[*BrainConfig](rawConf)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewBrain(ctx, deps, rawConf.ResourceName(), conf, logger)
+
+}
+
+func NewBrain(ctx context.Context, deps resource.Dependencies, name resource.Name, conf *BrainConfig, logger logging.Logger) (resource.Resource, error) {
+
+	cancelCtx, cancelFunc := context.WithCancel(context.Background())
+
+	s := &brainBrain{
+		name:       name,
+		logger:     logger,
+		cfg:        conf,
+		cancelCtx:  cancelCtx,
+		cancelFunc: cancelFunc,
+	}
+	return s, nil
+}
+
+func (s *brainBrain) Name() resource.Name {
+	return s.name
+}
+
+func (s *brainBrain) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *brainBrain) Close(context.Context) error {
+	// Put close code here
+	s.cancelFunc()
+	return nil
+}
